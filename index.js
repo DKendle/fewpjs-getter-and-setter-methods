ddcf27a0b42b3315a2e365d708cddfe0a7c3748e
DKendle <dina.kendle@gmail.com>
2021-06-15 19:18:12 -0400

    adds shells of methods

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your Circle class here
+class Circle {
+    constructor(radius){
+        this.radius = radius
+    }
+
+    get diameter(){
+
+    }
+    set diameter(){
+
+    }
+    get circumference(){
+
+    }
+    set circumference(){
+
+    }
+    get area(){
+
+    }
+    set area(){
+
+    }
+}
+
+//use Math.PI
